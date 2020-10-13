f12532f308c73a58732b8cdcf4ca840dbf91b762
Guy Carmeli <guyc@wix.com>
2020-10-13 14:55:43 +0300

    Fix crash

--- imagepicker/src/main/java/com/esafirm/imagepicker/view/SquareFrameLayout.kt
+++ imagepicker/src/main/java/com/esafirm/imagepicker/view/SquareFrameLayout.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
 
-class SquareFrameLayout(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+class SquareFrameLayout @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr) {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
